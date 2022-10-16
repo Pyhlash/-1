ca27bade7da472e891217e7c70c38479756444ce
Ivan lyashok <97744261+Pyhlash@users.noreply.github.com>
2022-10-17 00:05:55 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите a");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите b");
+        int    b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Среднее арифметическое: " + (double)((a + b) / 2));
+        Console.WriteLine("Среднее геометрическое: " + (double)(a * b));
+    }
+}
